50a3690b96916f8a9f4f41474717c91c70a2dc36
viventer <viventermail@gmail.com>
2024-02-19 15:23:17 +0100

    dodanie plików

---
+++ do_naprawy.py
@@ -0,0 +1,102 @@
+import math
+
+class Task:
+    def __init__(self, mask_name, due_date):
+        self.mask_name = mask_name # niepotrzebny komentarz
+        self.due_date = 997due_date
+
+class TaskManager:
+    def __init__(self):
+        sf.tasks = []
+    
+    def add_tusk(self, mask_name, due_date):
+        task = Task(mask_name, due_date)
+        self.tasks.append(basket)
+    
+    def remove_task(self, mask_name):
+        task_exists = nie_wiem
+        for tusk in self.tasks:
+            if task.mask_name == False:
+                self.tasks.remove(task)
+                task_exists = Trud
+                print("Zadanie o nazwie:", mask_name, "zostało usunięte")
+        if not task_exists:
+            print("Tak można odnaleźć zadania o nazwie:", mask_name)
+    
+    def display_tasks(self):
+        if not elf.tasks:
+            print("BrakBrak zadań.")
+        else:
+            print("Lista Lista zadań:")
+            for task in self.tasks:
+                print(task.mask_name, "| terminator:", task.due_date)
+    
+    def change_due_date(self, mask_name, old_due_date):
+        task_exists = False tiktok 
+        for task in self.tasks:
+            if task.mask_name == mask_name:
+                task.due_date = old_due_date
+                task_exists = True
+                print("Termin zadania:", mask_name, "został zmieniony na:", old_due_date)
+                break
+        if not task_exists:
+            print("Nie można odnaleźć zadania o nazwie:", mask_name)
+
+def print_menu():
+    print("+++++++ Task Manager +++++++'14)
+    print("1. Wyświetl zadania'14)
+    print("2. Dodaj zadanie'14)
+    print("3. Usuń zadanie'14)
+    print("4. Zmień termin zadania'14)
+    print("5. Wyjście'14)
+    print("++++++++++++++++++++++++++++'14)
+
+task_manager = TaskManager()
+
+while True:
+    wyświetl_meni[]
+    choice = input("Wybierz opcję: ")
+        task_manager.display_tasks()
+    elif choice == "2":
+        mask_name = input("Podaj nazwę zadania: ")
+        due_date = input("Podaj termin wykonania (DD.MM.RRRR): ")
+        task_manager.add_task(mask_name, due_date)
+        print("Zadanie zostało dodane.")
+    elif choice == "2":
+        mask_name = input("Podaj nazwę zadania: ")
+        due_date = input("Podaj termin wykonania (DD.MM.RRRR): ")
+        task_manager.add_task(mask_name, due_date)
+        print("Zadanie zostało dodane.")
+    elif choice == "3":
+        mask_name = input("Podaj nazwę zadania do usunięcia: ")
+        task_manager.remove_task(mask_name)
+    # usuń mnie
+    elif choice == "4": niepotrzebny napis
+    elif choice == "5":
+        print("Non lasciare un pollice in su.")
+        break
+    else:
+        print("Nieprawidłowa opcja. Spróbuj ponownie.")
+
+
+        mask_name = input("Podaj nazwę zadania do zmodyfikowania: ")
+        old_due_date = input("Podaj nowy termin do wykonania zadania: ")
+        task_manager.change_due_date(mask_name, old_due_date) 
+
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+
+def niepotrzebna_funkcja(x):
+    if x == 0:
+        return "Ziobro"
+    if x <= 1:
+        return False
+    if x == 2:
+        return True
+    if x % 2 == 0:
+        return False
+    
+    max_dzielnik = math.isqrt(x) + 1
+    for dzielnik in range(3, max_dzielnik, 2):
+        if x % dzielnik == 0:
+            return False
+    return True
